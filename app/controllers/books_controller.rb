be4b15d3065f469cef22080b822a2f95072f578d
PSzyszka <pawelszyszka26@wp.pl>
2018-05-17 12:28:43 +0200

    Updated books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  layout "books", only: [:index]
 
   def index
     @books = Book.includes(:genre, :author, :publisher).search(search_params)
@@ -8,39 +9,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
-  def new
-    @book = Book.new
-  end
-
-  def edit
-    @book = Book.find(params[:id])
-  end
-
-  def create
-    @book = Book.new(book_params)
-
-    if @book.save
-      redirect_to :index
-    else
-      redirect_to new_book_path
-    end
-  end
-
-  def update
-    @book = Book.find(params[:id])
-
-    if @book.update(book_params)
-      redirect_to @book
-    else
-      render 'edit'
-    end
-  end
-
   private
-  def book_params
-    params.require(:book).permit(:title, :description, :rating, :year_of_release, genre_attributes: [:name], author_attributes: [:first_name, :last_name])
-  end
-
   def search_params
     params.permit(:search)
   end
