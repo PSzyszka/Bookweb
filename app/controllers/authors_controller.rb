77c24f3aa2e837b5a23b4c48763ca01152d7cddf
PSzyszka <pawelszyszka26@wp.pl>
2018-05-17 12:26:28 +0200

    Udated show method in authors controller

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,39 +1,10 @@
 class AuthorsController < ApplicationController
-  # before_action :authenticate_admin!, only: [:new, :create, :update]
-
   def index
     @authors = Author.all
   end
 
   def show
     @author = Author.find(params[:id])
+    @genres = Genre.joins(:books).merge(Book.where(author: @author)).select(:id, :name).distinct
   end
-
-  # def new
-  #   @author = Author.new
-  # end
-
-  # def edit
-  #   @author = Author.find(params[:id])
-  # end
-
-  # def create
-  #   @author = Author.create(params[:author])
-  #   if @author.save
-  #     redirect_to 'index'
-  #   else
-  #     render 'new'
-  #   end
-  # end
-
-  # def update
-
-  # end
-
-  # def destroy
-  #   @author = Author.find(params[:id])
-  #   @author.destroy
-
-  #   redirect_to 'index'
-  # end
 end
