353dfdff3e389e5abea5cdbc3094147e6bad44fd
PSzyszka <pawelszyszka26@wp.pl>
2018-06-03 20:59:08 +0200

    Updating spec tests for books, genres controllers and rails helper

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,21 +1,67 @@
-require 'rails_helper'
+require "rails_helper"
 
 describe BooksController do
-  describe 'index' do
+  let!(:genre) { Genre.create(name: 'Action') }
+  let!(:publisher) { Publisher.create(name: 'Bookshop', city: 'Wroclaw', description: 'Publisher description') }
+  let!(:author) do
+    Author.create(
+      first_name: 'Alan',
+      last_name: 'Nowak',
+      description: 'Author description',
+      birth_date: Date.new(1994,3,15)
+    )
+  end
+  let!(:book1) do
+    Book.create(
+      title: 'Kill Bill',
+      description: 'Book description',
+      year_of_release: Date.new(2015,10,20),
+      original_language: 'Polish',
+      number_of_pages: 150,
+      translated_into: 'German',
+      author: author,
+      publisher: publisher,
+      genre: genre
+    )
   end
 
-  describe 'show' do
+  let!(:book2) do
+    Book.create(
+      title: 'Deadpool',
+      description: 'Book description',
+      year_of_release: Date.new(2020,8,25),
+      original_language: 'German',
+      number_of_pages: 230,
+      translated_into: 'English',
+      author: author,
+      publisher: publisher,
+      genre: genre
+    )
   end
 
-  describe 'new' do
+  describe 'GET index' do
+    it 'assigns books' do
+      get :index
+      expect(assigns(:books)).to eq([book1, book2])
+    end
+
+    it 'renders the index template' do
+      get :index
+      expect(response).to render_template('index')
+    end
   end
 
-  describe 'create' do
-  end
+  describe 'GET show' do
+    it 'assigns book' do
+      params = { id: book1.id }
+      get :show, params: params
+      expect(assigns(:book)).to eq(book1)
+    end
 
-  describe 'edit' do
-  end
-
-  describe 'update' do
+    it 'renders show template' do
+      params = { id: book1.id }
+      get :show, params: params
+      expect(response).to render_template('show')
+    end
   end
 end
--- spec/controllers/genres_controller_spec.rb
+++ spec/controllers/genres_controller_spec.rb
@@ -1,6 +1,31 @@
 require 'rails_helper'
 
 describe GenresController do
-  describe 'index' do
+  let!(:genre1) { Genre.create(name: 'Action') }
+  let!(:genre2) { Genre.create(name: 'Fiction') }
+  describe 'GET index' do
+    it 'assigns genres' do
+      get :index
+      expect(assigns(:genres)).to eq([genre1, genre2])
+    end
+
+    it 'renders the index template' do
+      get :index
+      expect(assigns(:genres)).to render_template('index')
+    end
+  end
+
+  describe 'GET books' do
+    it 'assigns genre' do
+      params = { id: genre1.id }
+      get :books, params: params
+      expect(assigns(:genre)).to eq(genre1)
+    end
+
+    it 'renders the books template' do
+      params = { id: genre1.id }
+      get :books, params: params
+      expect(assigns(:genre)).to render_template('books')
+    end
   end
 end
